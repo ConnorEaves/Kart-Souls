14080c1041236815edf845ab61aba79ebb7ebaf0
Josh Brown <S.Josh.Brown@Gmail.com>
2015-04-07 18:25:07 -0500

    implemented checkpoint system

---
+++ Assets/Scripts/CheckpointBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll){
+		KartLapTimer lapTimer = coll.GetComponent<KartLapTimer> ();
+
+		if (this.gameObject.GetComponent<BoxCollider>() == lapTimer.checkpoint[lapTimer.checkpointCounter])
+			lapTimer.HitCheckpoint ();
+	}
+}
---
+++ Assets/Scripts/KartLapTimer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KartLapTimer : MonoBehaviour {
+	public Collider[] checkpoint;
+	public int checkpointCounter;
+	private bool firstLap;
+	private float[] lapTime = new float[3];
+	private float startLap;
+	private int lapCounter;
+	// Use this for initialization
+	void Start () {
+		checkpointCounter = 0;
+		firstLap = true;
+		lapCounter = 0;
+		StartLap ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void HitCheckpoint() {
+
+		Debug.Log("checkpoint");
+
+		if (checkpointCounter == 0 && !firstLap)
+			FinishLap ();
+		if (checkpointCounter == checkpoint.Length - 1) {
+			checkpointCounter = 0;
+			firstLap = false;
+		}
+		else
+			checkpointCounter ++;
+	}
+
+	void StartLap(){
+		startLap = Time.realtimeSinceStartup;
+	}
+
+	void FinishLap(){
+		lapTime [lapCounter] = Time.realtimeSinceStartup - startLap;
+		Debug.Log (lapTime [lapCounter]);
+		lapCounter++;
+		StartLap ();
+
+	}
+}
