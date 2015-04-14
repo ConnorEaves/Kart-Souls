9be2009d5b5ea51daf412cd701985f8194f27228
Josh Brown <S.Josh.Brown@gmail.com>
2015-04-14 09:24:11 -0500

    implemented bars on the gate in one.unity

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gate : MonoBehaviour {
+	bool gateDown;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("GateToggle", 3.0f, 5.0f);
+		gateDown = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GateToggle()
+	{
+		if (gateDown) {
+			transform.Translate (-Vector3.forward * 8.0f);
+		}
+		else {
+			transform.Translate (Vector3.forward * 8.0f);
+		}
+		gateDown = !gateDown;
+	}
+
+}
