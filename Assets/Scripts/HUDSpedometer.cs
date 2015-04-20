80e7e19d66f27902e4335ec78c078c0e3df2bbe9
Kishotta <kishotta@gmail.com>
2015-04-20 16:14:17 -0500

    Spedometer

---
+++ Assets/Scripts/HUDSpedometer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDSpedometer : MonoBehaviour {
+
+	public GameObject PlayerKart;
+
+	KartController playerKartController;
+
+	void Awake () {
+		playerKartController = PlayerKart.GetComponent<KartController> ();
+	}
+
+	void Update () {
+		float speed = playerKartController.CurrentSpeed;
+		transform.rotation = Quaternion.Euler (0, 0, -Mathf.Abs (speed) * 2.0f);
+	}
+
+}
