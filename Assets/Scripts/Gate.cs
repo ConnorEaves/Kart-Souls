49211058106f377710e69ab51b0c7eef17e17bba
Josh Brown <S.Josh.Brown@Gmail.com>
2015-04-23 16:49:13 -0500

    gate movement on one.unity

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -3,25 +3,33 @@ using System.Collections;
 
 public class Gate : MonoBehaviour {
 	bool gateDown;
+	float startY;
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating ("GateToggle", 3.0f, 5.0f);
+		//InvokeRepeating ("GateToggle", 3.0f, 5.0f);
 		gateDown = true;
+		startY = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log (transform.position.y);
+		if (transform.position.y < startY && gateDown) {
+			GateToggle ();
+		} else if (transform.position.y > (startY + 8.0f) && !gateDown) {
+			GateToggle ();
+		}
+
+		if (gateDown){
+			transform.Translate(Vector3.forward * Time.deltaTime * 3);
+		}
+		else if (!gateDown){
+			transform.Translate(- Vector3.forward * Time.deltaTime * 3);
+		}
 	}
 
 	void GateToggle()
 	{
-		if (gateDown) {
-			transform.Translate (-Vector3.forward * 8.0f);
-		}
-		else {
-			transform.Translate (Vector3.forward * 8.0f);
-		}
 		gateDown = !gateDown;
 	}
 
