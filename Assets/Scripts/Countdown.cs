7012302310b024f09d01e26d06278cde001711b4
Tendain <christopher.waldon@yahoo.com>
2015-04-23 23:00:33 -0500

    Countdown beeps

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -7,6 +7,8 @@ public class Countdown : MonoBehaviour {
 	public GameObject[] Karts;
 
 	Text countdownText;
+	public AudioClip count;
+	public AudioClip go;
 
 	void Start () {
 		Karts = GameObject.FindGameObjectsWithTag ("kart");
@@ -23,15 +25,19 @@ public class Countdown : MonoBehaviour {
 		}
 
 		countdownText.text = "3";
+		AudioSource.PlayClipAtPoint (count, Camera.main.transform.position);
 		yield return new WaitForSeconds (1.0f);
 		countdownText.text = "2";
+		AudioSource.PlayClipAtPoint (count, Camera.main.transform.position);
 		yield return new WaitForSeconds (1.0f);
 		countdownText.text = "1";
+		AudioSource.PlayClipAtPoint (count, Camera.main.transform.position);
 		yield return new WaitForSeconds (1.0f);
 		foreach (GameObject kart in Karts) {
 			kart.GetComponent<KartController> ().enabled = true;
 		}
 		countdownText.text = "GO!";
+		AudioSource.PlayClipAtPoint (go, Camera.main.transform.position);
 		yield return new WaitForSeconds (2.0f);
 		countdownText.text = "";
 		
