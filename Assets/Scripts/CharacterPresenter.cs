0ac228ab38bb07309da6a6998ff84a1173601e31
Kishotta <kishotta@gmail.com>
2015-04-26 18:23:21 -0500

    Added finish line markers to tracks
    tried making the karts rotate in the main menu as you highlight their
    names, but it's just too involved/complicated to mess with

---
+++ Assets/Scripts/CharacterPresenter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharacterPresenter : MonoBehaviour {
+
+	public Canvas CharSelectCanvas;
+
+	void Update () {
+		if(CharSelectCanvas.enabled) {
+			gameObject.SetActive(true);
+		} else {
+			gameObject.SetActive (false);
+		}
+	}
+}
